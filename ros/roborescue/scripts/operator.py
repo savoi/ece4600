8338914e5ceabd4d69af14fd944d116f9dfd022f
Josh Greenberg <castro.greenberg@gmail.com>
2015-02-16 12:50:02 -0600

    Made socket server more resilient
    Reopens socket when client disconnects.

--- ros/roborescue/scripts/operator.py
+++ ros/roborescue/scripts/operator.py
@@ -4,6 +4,7 @@
 # Subscribes to status messages received from the arduino
 
 import rospy
+import select
 import socket
 import sys
 
@@ -14,45 +15,73 @@ class OperatorComm():
 
 	PACKET_SIZE = 5				# Control message size in bytes
 	LISTEN_PORT = 6969			# Operator station control msgs
-	BUFFER_SIZE = 64
-
+	BUFFER_SIZE = 256
+	
+	connections = []		# list of socket clients
 	pub = None				# ros publisher
 	sub = None				# ros subscriber
 	sock = None				# socket 
 	conn = None				# client connection
+	outgoing_msgs = [None]
+	w_sockets = []
+	r_sockets = []
+	err_sockets = []
 
 	# Initialize ROS publisher, subscriber and socket connection
 	def __init__(self):
 		rospy.init_node('operator', anonymous=False)
-		# Create TCP/IP socket
-		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		host = socket.gethostname()
-		self.sock.bind(('', self.LISTEN_PORT))
-		self.sock.listen(1)
-		rospy.loginfo('Listening on port %s' %self.LISTEN_PORT)
-		self.conn, addr = self.sock.accept()
-		rospy.loginfo('Connection established with {0}'.format(addr))
-
+		self.rate = rospy.Rate(1)
 		# Initialize ROS pub and sub
 		self.pub = rospy.Publisher('control_msgs', String, queue_size=20)
 		self.sub = rospy.Subscriber('status_msgs', String, self.handleArduinoData)
 		rate = rospy.Rate(10)
+		self.initSocket()
+		
+	# Create TCP/IP socket
+	def initSocket(self):
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		host = socket.gethostname()
+		self.sock.bind(('', self.LISTEN_PORT))
+		self.sock.listen(1)
+		self.connections.append(self.sock)
+		rospy.loginfo('Server listening on port {0}'.format(self.LISTEN_PORT))
 
 	# Handler for sending status data to operator station from arduino
 	def handleArduinoData(self, data):
-		bytes_sent = self.conn.send(str(data))
-		rospy.loginfo('sent "{0}" bytes'.format(bytes_sent))
+		self.outgoing_msgs[0] = data.data
 
 	def startNode(self):
-		try:	
-			while not rospy.is_shutdown():
-				data = self.conn.recv(self.BUFFER_SIZE)
-				if data:
-					rospy.loginfo('received "{0}"'.format(data))
-					self.pub.publish(data)
-					self.parsedata(data)
-		finally:
-			self.conn.close()
+		while not rospy.is_shutdown():
+			self.r_sockets, self.w_sockets, self.err_sockets = select.select(self.connections, [], [])
+
+			for sock in self.w_sockets:
+				try:
+					sock.send("HELLOE")
+				except:
+					rospy.logwarn('ERROR sending data')
+		
+			for sock in self.r_sockets:
+				# new connection
+				if sock == self.sock:
+					conn, addr = sock.accept()
+					self.connections.append(conn)
+					rospy.loginfo('Client connected: {0}'.format(addr))
+				# data incoming
+				else:
+					try:
+						data = sock.recv(self.BUFFER_SIZE)
+						if data:
+							rospy.logdebug('received "{0}"'.format(data))
+							self.pub.publish(data)
+							self.parsedata(data)
+							bytes_sent = sock.send(str(self.outgoing_msgs[0]))
+							rospy.loginfo('{0} bytes sent'.format(bytes_sent))
+					except:
+						rospy.loginfo('Client disconnected')
+						sock.close()
+						self.connections.remove(sock)
+						continue
+			self.rate.sleep()
 
 	def parsedata(self, data):
 		ba = Bits(bytes=data)	
