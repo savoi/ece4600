6e1440bbff5f6e94fc96b8e17f0b53712a8e4b6c
Josh Greenberg <castro.greenberg@gmail.com>
2015-02-26 17:17:25 -0600

    Refactored video server to use multiprocessing library
    - ROS process launches worker processes for each camera.
    - Each worker process launches a GStreamer process.
    - Implemented video toggling to allow the operator to turn a camera
    on/off.

--- ros/roborescue/scripts/videoserver.py
+++ ros/roborescue/scripts/videoserver.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 
+import multiprocessing as mp
 import os
 import rospy
-import shlex
 import signal
-import subprocess
+import time
+import videopipelines
 
 from proto import ControlPacket, PacketError
 from std_msgs.msg import String
@@ -17,9 +18,10 @@ class VideoServer:
 				 "udpsink host=192.168.0.53 port=9000")
 
 	cam2pipe1 = ("gst-launch v4l2src "
-				 "device=/dev/v4l/by-id/usb-046d_HD_Webcam_C525_36C31260-video-index0 "
-				 "! 'image/jpeg,width=320,height=240' ! rtpjpegpay ! "
-				 "udpsink host=192.168.0.53 port=9000")
+				 "device=/dev/v4l/by-id/usb-OmniVision_Technologies__Inc._USB_Camera-B3.04.06.1-video-index0 "
+				 "! 'video/x-raw-yuv,width=320,height=240' ! ffmpegcolorspace ! "
+				 "jpegenc ! rtpjpegpay ! "
+				 "udpsink host=192.168.0.53 port=9002")
 
 	DEVICE_ID_PATH = '/dev/v4l/by-id'
 	HWID_CAM1 = 'usb-046d_HD_Webcam_C525_36C31260-video-index0'
@@ -33,44 +35,61 @@ class VideoServer:
 
 	# Initialize ROS subscriber
 	def __init__(self):
-		self.proc1 = None
-		self.proc2 = None
+		self.workerinit_1()
+		self.workerinit_2()
 		rospy.init_node('videoserver', anonymous=False)
+		rospy.loginfo('Started videoserver node')
 		self.sub = rospy.Subscriber('control_msgs', String, self.handleControlPacket)
+		rospy.loginfo('Waiting for operator controls...')
 		rospy.spin()
 	
 	# Toggles cameras one and two on/off
 	def handleControlPacket(self, data):
-		packet = ControlPacket(data.data)
-		if packet.camone == '11':
-			if not self.proc1:			# server not running
-				rospy.loginfo('Starting video server 1')	
-				self.init_camera1()	
-			else:						# server running
-				rospy.loginfo('Killing video server 1')	
-				os.kill(self.proc1.pid, signal.SIGTERM)
-				self.proc1 = None
-		if packet.camtwo == '11':
-			if not self.proc2:			# server not running
-				rospy.loginfo('Starting video server 2')	
-				self.init_camera2()	
-			else:						# server running
-				rospy.loginfo('Killing video server 2')	
-				os.kill(self.proc2.pid, signal.SIGTERM)
-				self.proc2 = None
+		try:
+			packet = ControlPacket(data.data)
+			if packet.camone == '11':
+				# video server 1 not running
+				if not self.vidworker_1.is_alive():
+					rospy.loginfo('Starting video server 1')	
+					self.vidworker_1.start()
+				# kill video server 1
+				else:
+					rospy.loginfo('Killing video server 1')	
+					self.event1.set()
+					time.sleep(3)
+					rospy.loginfo('{0} is alive: {1}'.format(self.vidworker_1.name, self.vidworker_1.is_alive()))	
+					self.event1.clear()
+					self.workerinit_1()
+			if packet.camtwo == '11':
+				# video server 2 not running
+				if not self.vidworker_2.is_alive():
+					rospy.loginfo('Starting video server 2')	
+					self.vidworker_2.start()
+				# kill video server 2
+				else:
+					rospy.loginfo('Killing video server 2')	
+					self.event2.set()
+					time.sleep(3)
+					rospy.loginfo('{0} is alive: {1}'.format(self.vidworker_2.name, self.vidworker_2.is_alive()))	
+					self.event2.clear()
+					self.workerinit_2()
+		except PacketError:
+			rospy.logwarn('Packet parsing error.')
+
+	# Initialize first camera process template and event signal
+	def workerinit_1(self):
+		self.event1 = mp.Event()
+		self.vidworker_1 = mp.Process(name='vidworker_1',
+									  target=videopipelines.worker,
+									  args=(self.cam1pipe1, self.event1))
+
+	# Initialize second camera process template and event signal
+	def workerinit_2(self):
+		self.event2 = mp.Event()
+		self.vidworker_2 = mp.Process(name='vidworker_2',
+									  target=videopipelines.worker,
+									  args=(self.cam2pipe1, self.event2))
 	
-	def init_camera1(self):
-		args = ["/usr/bin/python", "/home/ubuntu/catkin_ws/src/roborescue/scripts/videopipelines.py", "1"]
-		self.proc1 = subprocess.Popen(args, shell=False,
-									  close_fds=True,
-									  preexec_fn=os.setsid,)
-		
-	def init_camera2(self):
-		args = ["/usr/bin/python", "/home/ubuntu/catkin_ws/src/roborescue/scripts/videopipelines.py", "2"]
-		self.proc2 = subprocess.Popen(args, shell=False,
-									  close_fds=True,
-									  preexec_fn=os.setsid,)
-		
 if __name__ == "__main__":
 	try:
 		vs = VideoServer()
